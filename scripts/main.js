efeb084ae013eafe160743cd4b77e36b816b701c
CharisseL <charisse8@gmail.com>
2014-10-23 14:59:15 -0500

    find temp by zip working

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,5 @@
-var url = 'http://api.wunderground.com/api/2ffba1e154f1d0c5/forecast10day/q/TN/Nashville.json';
+var zipcode = '37217';
+var url = 'http://api.wunderground.com/api/2ffba1e154f1d0c5/forecast10day/q/' + zipcode + '.json';
 
 function addItemToList($list, temp){
 	var $li =document.createElement('li');
@@ -6,14 +7,19 @@ function addItemToList($list, temp){
   var $high = document.createElement('p');
   var $low = document.createElement('p');
   $day.innerHTML = temp.date.weekday;
-  $high.innerHTML = temp.high.fahrenheit + "&deg f";
-  $low.innerHTML = temp.low.fahrenheit + "&deg f";
+  $high.innerHTML = 'High: ' + temp.high.fahrenheit + "&deg f";
+  $low.innerHTML = 'Low: ' + temp.low.fahrenheit + "&deg f";
   $li.appendChild($day);
   $li.appendChild($high);
   $li.appendChild($low);
   $list.appendChild($li);
 }
+function newZip(city){
+  var $newCity = document.getElementById('newCity');
+  $newCity.innerHTML = city;
+}
 
+	
 function getJSONP(url, cbName){
   var $script = document.createElement('script');
   $script.src = url + '?callback=' + cbName;
@@ -27,9 +33,17 @@ function showData(data){
     addItemToList($ul, forecast[i]);
     console.log('FC', forecast[i]);
   }
+	var city = data.location.city;
+  newCity(city);
 }
 document.addEventListener('DOMContentLoaded', function(){
-  getJSONP(url, 'showData')
+	var $form = document.getElementById('enterZip');
+	var $userZip = $form.querySelector('input[type=text]');
+	$form.addEventListener('submit', function(event){
+		event.preventDefault();	
+	var url = 'http://api.wunderground.com/api/2ffba1e154f1d0c5/forecast10day/q/' + $userZip.value + '.json';
+	getJSONP(url, 'showData')
+  });
 });
 
 
